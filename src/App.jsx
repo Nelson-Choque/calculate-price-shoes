9aa16e313ec7bec8e98f7f98e7df94694a1ed011
Nelson Choque <nelsonchoque03@gmail.com>
2023-08-08 22:48:40 -0500

    test 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import { useRef, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import "./App.css";
-// import { AddProductForm } from "./componentes/addProductForm.jsx";
+import { AddProductForm } from "./componentes/addProductForm.jsx";
 
 function App() {
   const inputPesoMedias = useRef();
